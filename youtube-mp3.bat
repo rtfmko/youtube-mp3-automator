4e146ccff13bbc4f9b1cb0fa81479df488370bc8
rtfmko <55385401+rtfmko@users.noreply.github.com>
2025-08-18 16:01:30 +0300

    update youtube bat
    Added get .bat location to update
    Added read-only

--- youtube-mp3.bat
+++ youtube-mp3.bat
@@ -6,16 +6,19 @@ set "updaterUrl=https://raw.githubusercontent.com/rtfmko/youtube-mp3-automator/m
 
 :: Local script directory
 set "scriptDir=%LOCALAPPDATA%\yt-dlp"
+set "batDir=%~dp0"
 set "updaterPath=%scriptDir%\updater.ps1"
 
 :: Ensure script directory exists
 if not exist "%scriptDir%" mkdir "%scriptDir%"
 
+if "%batDir:~-1%"=="\" set "batDir=%batDir:~0,-1%"
+
 :: Check if updater.ps1 exists
 if not exist "%updaterPath%" (
     echo 🔄 updater.ps1 not found, downloading...
-    powershell -NoProfile -Command ^
-        "try { Invoke-WebRequest -Uri '%updaterUrl%' -OutFile '%updaterPath%' -UseBasicParsing -ErrorAction Stop } catch { exit 1 }"
+	powershell -NoProfile -Command ^
+		"try { Invoke-WebRequest -Uri '%updaterUrl%' -OutFile '%updaterPath%' -UseBasicParsing -ErrorAction Stop; Set-ItemProperty -Path '%updaterPath%' -Name IsReadOnly -Value $true } catch { exit 1 }"
 
     if errorlevel 1 (
         echo ❌ Failed to download updater.ps1
@@ -25,6 +28,6 @@ if not exist "%updaterPath%" (
 )
 
 :: Run updater.ps1 and pass scriptDir as argument
-powershell -NoProfile -ExecutionPolicy Bypass -File "%updaterPath%" -scriptDir "%scriptDir%"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%updaterPath%" -scriptDir "%scriptDir%" -batDir "%batDir%"
 
 endlocal
